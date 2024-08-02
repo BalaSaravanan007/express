95200c3c3a2d413eed4c3681dc2643ad6f8d93d6
Anusuya-Balakrishnan <anusuyabalakrishnan01@gmail.com>
2024-08-02 17:43:30 +0530

    Error Handling Finished!

--- Controller/errorController.js
+++ Controller/errorController.js
@@ -1,4 +1,21 @@
-// GLOBAL ERROR HANDLING MIDDLEWARE
+const AppError = require('./../Utils/appError');
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.value} for ${err.path}`;
+  return new AppError(message, 400);
+};
+
+const handleDuplicateFieldsDB = (err) => {
+  // console.log(err.keyValue.name);
+  const message = `Duplicate Key '${err.keyValue.name}' already exists!!`;
+  return new AppError(message, 400);
+};
+
+const handleValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+  const message = `Invalid input data ${errors.join('. ')}`;
+  return new AppError(message, 400);
+};
 
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
@@ -7,17 +24,18 @@ const sendErrorDev = (err, res) => {
     message: err.message,
     stack: err.stack,
   });
+  // console.log(err.message);
 };
 
 const sendErrorProd = (err, res) => {
   if (err.isOperational) {
-    // 1) Operational err: we can send details about the error to the user
+    // 1) Operational err: we can send details about the error to the user.
     res.status(err.statusCode).json({
       status: err.status,
       message: err.message,
     });
   } else {
-    // 2) Programming err: we don't need
+    // 2) Programming err: we don't need to send any information to the user.
     console.error('ERROR: 💥', err);
 
     res.status(500).json({
@@ -27,8 +45,10 @@ const sendErrorProd = (err, res) => {
   }
 };
 
+// GLOBAL ERROR HANDLING MIDDLEWARE
+
 module.exports = (err, req, res, next) => {
-  console.log(err.stack);
+  //console.log(err.stack);
 
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
@@ -36,6 +56,13 @@ module.exports = (err, req, res, next) => {
   if (process.env.NODE_ENV == 'development') {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV == 'production') {
-    sendErrorProd(err, res);
+    let error = { ...err };
+
+    if (err.name === 'CastError') error = handleCastErrorDB(error);
+    if (err.code === 11000) error = handleDuplicateFieldsDB(error);
+    if (err.name === 'ValidationError') error = handleValidationErrorDB(error);
+    // console.log(err);
+
+    sendErrorProd(error, res);
   }
 };
--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
+process.on('uncaughtException', (err) => {
+  console.log(err.name, err.message);
+  console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 
 const DB = process.env.DATABASE;
@@ -40,6 +46,14 @@ mongoose
 
 // Starting The Server
 const port = process.env.PORT || 4200;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`Server running on the port ${port}`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+  server.close(() => {
+    process.exit(1);
+  });
 });
